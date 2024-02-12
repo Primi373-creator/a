1b2e680b2a4c49f16c856de9f635e65be6de712c
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-02-12 19:01:51 +0100

    Update config.js

--- config.js
+++ config.js
@@ -21,5 +21,6 @@ module.exports = {
   TENORAPI: process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c",
   PORT: process.env.PORT || "9000",
   PREFIX: process.env.PREFIX || ".",
-  ANTILINK_MSG: process.env.ANTILINK_MSG || '',
+  logmsg: process.env.LOGMSG || ".",
+  antilinkmsg: process.env.ANTILINK_MSG || '',
 };
